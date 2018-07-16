e489580881f84825018473c70309c064450bc55a
wjf-yht <549719832@qq.com>
2018-07-16 10:50:17 +0800

    1

--- jt-manage/jt-manage-web/src/main/java/com/jt/manage/web/controller/PageController.java
+++ jt-manage/jt-manage-web/src/main/java/com/jt/manage/web/controller/PageController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/page")
 public class PageController {
 	
 	@RequestMapping("/{pageName}")
