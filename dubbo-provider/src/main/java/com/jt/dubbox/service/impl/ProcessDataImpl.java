378e9d46816ede6646d5082ea9d190fb69a29606
wjeffeng <549719832@qq.com>
2018-09-27 21:11:23 +0800

    123

--- dubbo-provider/src/main/java/com/jt/dubbox/service/impl/ProcessDataImpl.java
+++
@@ -1,9 +0,0 @@
-package com.jt.dubbox.service.impl;
-
-import com.jt.dubbox.api.IProcessData;
-
-public class ProcessDataImpl implements IProcessData {
-	public String hello(String name) {
-		return "service1 hello : " + name;
-	}
-}
